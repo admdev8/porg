1002cb42267b877a0a4e439a4647a6404f30cd3f
Dennis Yurichev <dennis@yurichev.com>
2013-06-30 19:50:02 +0300

    =copyright/licensing information added

--- test1.c
+++ test1.c
@@ -1,3 +1,18 @@
+/*
+ *  _ __   ___  _ __ __ _ 
+ * | '_ \ / _ \| '__/ _` |
+ * | |_) | (_) | | | (_| |
+ * | .__/ \___/|_|  \__, |
+ * | |               __/ |
+ * |_|              |___/
+ *
+ * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2013
+ *
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.
+ *
+ */
+
 #include <stdio.h>
 
 #include "strbuf.h"
